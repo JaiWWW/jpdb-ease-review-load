af4b3e7eb2932fce90e5f28dedbe9a0d8df1f863
Jai <108700712+JaiWWW@users.noreply.github.com>
2024-12-24 01:42:02 +0000

    Add try block
    Wrapped the whole main function in a try block to reinstate the body in case something goes wrong. Not sure why the diffchecker can't figure out most of it is the same except with one extra indent level.... but that's the only thing I did (I deliberately did this branch in 2 commits to make the diffchecker easier to read on the first one)

--- script.user.js
+++ script.user.js
@@ -53,105 +53,113 @@ if (DEBUG) {console.log('[DEBUG - Ease Review Load] Body hidden') }
     debugLog(`Trimmed url: ${url}`);
 
     function main() {
-        // Span element in the nav holding the due/new count (Pretty sure this exists on all jpdb pages?)
-        const navDCSpan = document.querySelector('div.nav a.nav-item > span');
-        if (navDCSpan) {
-            docObserver.disconnect(); // To avoid double triggering while the function is running
-            debugLog('Nav due count element found, docObserver disconnected');
+        try { // To make sure we restore visibility in case something goes wrong
 
-            const rawDueCount = navDCSpan.style.color === 'red' ? Number(navDCSpan.textContent) : 0;
-            let elementsToUpdate = [navDCSpan];
-
-            // I thought I wouldn't get to use fallthrough in this script :D
-            const containerDCSpan = document.querySelector('div.container > p > span:not(:only-of-type)');
-            switch (url.slice(-7)) { // last 7 characters
-                case '/review':
-                    if (!getItem('FinishedBlockHTML')) setUpFinishedPageTemplate();
-                case 'jpdb.io':
-                case 'o/learn':
-                    if (containerDCSpan) { // Doesn't exist except on the "You've finished your review session" page
-                        debugLog('Container due count element found');
-                        elementsToUpdate.push(containerDCSpan);
-                    }
-            }
-
-            /* Old bad code which I replaced with the beautiful fallthrough above:
-            if (url.endsWith('.io') || url.endsWith('/learn')) {
-                // Span element in a p in the container holding the due count
-                const containerDCSpan = document.querySelector('div.container > p > span');
-                debugLog('Container due count element found (homepage or /learn)');
-                elementsToUpdate.push(containerDCSpan);
-            } else if (url.endsWith('/review')) {
-                // Span element in a p in the container holding the due count
+            // Span element in the nav holding the due/new count (Pretty sure this exists on all jpdb pages?)
+            const navDCSpan = document.querySelector('div.nav a.nav-item > span');
+            if (navDCSpan) {
+                docObserver.disconnect(); // To avoid double triggering while the function is running
+                debugLog('Nav due count element found, docObserver disconnected');
+    
+                const rawDueCount = navDCSpan.style.color === 'red' ? Number(navDCSpan.textContent) : 0;
+                let elementsToUpdate = [navDCSpan];
+    
+                // I thought I wouldn't get to use fallthrough in this script :D
                 const containerDCSpan = document.querySelector('div.container > p > span:not(:only-of-type)');
-                if (containerDCSpan) { // Doesn't exist except on the "You've finished your review session" page
-                    debugLog('Container due count element found (finished review session page)');
+                switch (url.slice(-7)) { // last 7 characters
+                    case '/review':
+                        if (!getItem('FinishedBlockHTML')) setUpFinishedPageTemplate();
+                    case 'jpdb.io':
+                    case 'o/learn':
+                        if (containerDCSpan) { // Doesn't exist except on the "You've finished your review session" page
+                            debugLog('Container due count element found');
+                            elementsToUpdate.push(containerDCSpan);
+                        }
+                }
+    
+                /* Old bad code which I replaced with the beautiful fallthrough above:
+                if (url.endsWith('.io') || url.endsWith('/learn')) {
+                    // Span element in a p in the container holding the due count
+                    const containerDCSpan = document.querySelector('div.container > p > span');
+                    debugLog('Container due count element found (homepage or /learn)');
                     elementsToUpdate.push(containerDCSpan);
-                    if (!getItem('FinishedBlockHTML')) setUpFinishedPageTemplate();
+                } else if (url.endsWith('/review')) {
+                    // Span element in a p in the container holding the due count
+                    const containerDCSpan = document.querySelector('div.container > p > span:not(:only-of-type)');
+                    if (containerDCSpan) { // Doesn't exist except on the "You've finished your review session" page
+                        debugLog('Container due count element found (finished review session page)');
+                        elementsToUpdate.push(containerDCSpan);
+                        if (!getItem('FinishedBlockHTML')) setUpFinishedPageTemplate();
+                    }
                 }
-            }
-            */
-
-            if (!Activated && rawDueCount > ACTIVATION_THRESHOLD) {
-                // ^ requirements for activating the due count reduction
-                setItem('InitialRawDueCount', rawDueCount);
-                setItem('Activated', true);
-                InitialRawDueCount = rawDueCount;
-                Activated = true;
-                debugLog('Activated displayed due count reduction');
-            } if (Activated) { // not else if, because this block still needs to run if Activated was just enabled in the previous block
-                // We are going to calculate and show the displayed due count in this block so I will define it here
-                let displayedDueCount;
-                if (rawDueCount <= Math.max(0, InitialRawDueCount - RESTRICTED_MAX)) {
-                    // Reduced review pile is down to 0
-                    debugLog('Finished reduced review pile');
-                    if (rawDueCount > ACTIVATION_THRESHOLD) {
-                        // ^ requirement for re-initialising the due count reduction
+                */
+    
+                if (!Activated && rawDueCount > ACTIVATION_THRESHOLD) {
+                    // ^ requirements for activating the due count reduction
+                    setItem('InitialRawDueCount', rawDueCount);
+                    setItem('Activated', true);
+                    InitialRawDueCount = rawDueCount;
+                    Activated = true;
+                    debugLog('Activated displayed due count reduction');
+                } if (Activated) { // not else if, because this block still needs to run if Activated was just enabled in the previous block
+                    // We are going to calculate and show the displayed due count in this block so I will define it here
+                    let displayedDueCount;
+                    if (rawDueCount <= Math.max(0, InitialRawDueCount - RESTRICTED_MAX)) {
+                        // Reduced review pile is down to 0
+                        debugLog('Finished reduced review pile');
+                        if (rawDueCount > ACTIVATION_THRESHOLD) {
+                            // ^ requirement for re-initialising the due count reduction
+                            setItem('InitialRawDueCount', rawDueCount); // Don't need to update script copy of the variable as we won't use it again
+                            displayedDueCount = RESTRICTED_MAX; // Maybe set to 0 for user satisfaction until they click to continue reviewing?
+                            debugLog('Due reviews still over threshold; reactivated reduction');
+                        } else { // Deactivate
+                            setItem('Activated', false);
+                            Activated = false;
+                            displayedDueCount = rawDueCount;
+                            debugLog('Due count brought below threshold; deactivated reduction');
+                        }
+    
+                        if (rawDueCount > 0) showFinishedSessionPage(displayedDueCount);
+                        // otherwise the native reviews finished page will come up anyway
+    
+                    } else if (rawDueCount > InitialRawDueCount) {
+                        // Artificially increase initial due count to avoid displayed count going above RESTRICTED_MAX
                         setItem('InitialRawDueCount', rawDueCount); // Don't need to update script copy of the variable as we won't use it again
-                        displayedDueCount = RESTRICTED_MAX; // Maybe set to 0 for user satisfaction until they click to continue reviewing?
-                        debugLog('Due reviews still over threshold; reactivated reduction');
-                    } else { // Deactivate
-                        setItem('Activated', false);
-                        Activated = false;
-                        displayedDueCount = rawDueCount;
-                        debugLog('Due count brought below threshold; deactivated reduction');
+                        displayedDueCount = RESTRICTED_MAX;
+                        debugLog('Due count exceeded that at time of activation; recalibrated reduction');
+                    } else { // 0 < reduced review pile due count <= RESTRICTED_MAX
+                        // Displayed due count changes will mirror raw due count changes relative to the initial raw due count
+                        debugLog('Due count in expected range');
+                        displayedDueCount = RESTRICTED_MAX - (InitialRawDueCount - rawDueCount);
                     }
-
-                    if (rawDueCount > 0) showFinishedSessionPage(displayedDueCount);
-                    // otherwise the native reviews finished page will come up anyway
-
-                } else if (rawDueCount > InitialRawDueCount) {
-                    // Artificially increase initial due count to avoid displayed count going above RESTRICTED_MAX
-                    setItem('InitialRawDueCount', rawDueCount); // Don't need to update script copy of the variable as we won't use it again
-                    displayedDueCount = RESTRICTED_MAX;
-                    debugLog('Due count exceeded that at time of activation; recalibrated reduction');
-                } else { // 0 < reduced review pile due count <= RESTRICTED_MAX
-                    // Displayed due count changes will mirror raw due count changes relative to the initial raw due count
-                    debugLog('Due count in expected range');
-                    displayedDueCount = RESTRICTED_MAX - (InitialRawDueCount - rawDueCount);
-                }
-
-                // If user got to 0 displayed reviews and is now beneath ACTIVATION_THRESHOLD, we will have deactivated since
-                // the last time we checked Activated, so we need to check again to make sure we don't update when deactivated
-                if (Activated) {
-                    // Overwrite due counters
-                    debugLog('Overwriting counters');
-                    for (const element of elementsToUpdate) {
-                        element.textContent = displayedDueCount;
-                    }
-                    debugLog('Successfully overwrote counters');
-
-                    if (url.endsWith('/review')) {
-                        navObserver.observe(navDCSpan, { characterData: true, subtree: true });
-                        debugLog('navObserver set');
+    
+                    // If user got to 0 displayed reviews and is now beneath ACTIVATION_THRESHOLD, we will have deactivated since
+                    // the last time we checked Activated, so we need to check again to make sure we don't update when deactivated
+                    if (Activated) {
+                        // Overwrite due counters
+                        debugLog('Overwriting counters');
+                        for (const element of elementsToUpdate) {
+                            element.textContent = displayedDueCount;
+                        }
+                        debugLog('Successfully overwrote counters');
+    
+                        if (url.endsWith('/review')) {
+                            navObserver.observe(navDCSpan, { characterData: true, subtree: true });
+                            debugLog('navObserver set');
+                        }
                     }
                 }
+    
+                // Remove the style element to restore visibility
+                style.remove();
+                debugLog('Body reinstated');
+    
             }
-
+        } catch(e) {
             // Remove the style element to restore visibility
             style.remove();
-            debugLog('Body reinstated');
-
+            debugLog('Body reinstated because something went wrong:');
+            throw e
         }
     }
 
