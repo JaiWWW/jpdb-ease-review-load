90924af5da2c727cbf2f9941105669f1455bee46
Jai <108700712+JaiWWW@users.noreply.github.com>
2024-12-24 01:36:37 +0000

    Reorganised observer logic
    Previous patch didn't fix the issue so I did a full rework of the logic this time, hopefully it works this time

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JPDB Ease Review Load
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  If you have lots of reviews due, make it look like you have less so it's easier to manage
 // @author       JaiWWW
 // @match        https://jpdb.io/*
@@ -21,7 +21,6 @@ const DEBUG = false; // Enable debug logs
 
 /////////////////////////////////////////////////////////
 // TO DO                                               //
-// Figure out what's going on with comment on line ~97 //
 // Move user settings into jpdb settings page          //
 /////////////////////////////////////////////////////////
 
@@ -56,38 +55,51 @@ if (DEBUG) {console.log('[DEBUG - Ease Review Load] Body hidden') }
     function main() {
         // Span element in the nav holding the due/new count (Pretty sure this exists on all jpdb pages?)
         const navDCSpan = document.querySelector('div.nav a.nav-item > span');
-        let elementsToUpdate = [navDCSpan];
+        if (navDCSpan) {
+            docObserver.disconnect(); // To avoid double triggering while the function is running
+            debugLog('Nav due count element found, docObserver disconnected');
 
-        if (url.endsWith('.io') || url.endsWith('/learn')) {
-            // Span element in a p in the container holding the due count
-            const containerDCSpan = document.querySelector('div.container > p > span');
-            debugLog('Container due count element found (homepage or /learn)');
-            elementsToUpdate.push(containerDCSpan);
-        } else if (url.endsWith('/review')) {
-            // Span element in a p in the container holding the due count
-            const containerDCSpan = document.querySelector('div.container > p > span:not(:only-of-type)');
-            if (containerDCSpan) { // Doesn't exist except on the "You've finished your review session" page
-                debugLog('Container due count element found (finished review session page)');
-                elementsToUpdate.push(containerDCSpan);
-                if (!getItem('FinishedBlockHTML')) setUpFinishedPageTemplate();
-            }
-        }
-
-        if (elementsToUpdate[0]) {
-            debugLog('Nav due count element found');
             const rawDueCount = navDCSpan.style.color === 'red' ? Number(navDCSpan.textContent) : 0;
+            let elementsToUpdate = [navDCSpan];
 
-            // Disconnect the observer as the target element has been found
-            if (docObserver) {docObserver.disconnect(); debugLog('docObserver disconnected')}
+            // I thought I wouldn't get to use fallthrough in this script :D
+            const containerDCSpan = document.querySelector('div.container > p > span:not(:only-of-type)');
+            switch (url.slice(-7)) { // last 7 characters
+                case '/review':
+                    if (!getItem('FinishedBlockHTML')) setUpFinishedPageTemplate();
+                case 'jpdb.io':
+                case 'o/learn':
+                    if (containerDCSpan) { // Doesn't exist except on the "You've finished your review session" page
+                        debugLog('Container due count element found');
+                        elementsToUpdate.push(containerDCSpan);
+                    }
+            }
+
+            /* Old bad code which I replaced with the beautiful fallthrough above:
+            if (url.endsWith('.io') || url.endsWith('/learn')) {
+                // Span element in a p in the container holding the due count
+                const containerDCSpan = document.querySelector('div.container > p > span');
+                debugLog('Container due count element found (homepage or /learn)');
+                elementsToUpdate.push(containerDCSpan);
+            } else if (url.endsWith('/review')) {
+                // Span element in a p in the container holding the due count
+                const containerDCSpan = document.querySelector('div.container > p > span:not(:only-of-type)');
+                if (containerDCSpan) { // Doesn't exist except on the "You've finished your review session" page
+                    debugLog('Container due count element found (finished review session page)');
+                    elementsToUpdate.push(containerDCSpan);
+                    if (!getItem('FinishedBlockHTML')) setUpFinishedPageTemplate();
+                }
+            }
+            */
 
             if (!Activated && rawDueCount > ACTIVATION_THRESHOLD) {
-                // ^ requirements for initialising the due count reduction
+                // ^ requirements for activating the due count reduction
                 setItem('InitialRawDueCount', rawDueCount);
                 setItem('Activated', true);
                 InitialRawDueCount = rawDueCount;
                 Activated = true;
                 debugLog('Activated displayed due count reduction');
-            } if (Activated) { // not else if, because this block always needs to run if Activated regardless of whether the previous one did
+            } if (Activated) { // not else if, because this block still needs to run if Activated was just enabled in the previous block
                 // We are going to calculate and show the displayed due count in this block so I will define it here
                 let displayedDueCount;
                 if (rawDueCount <= Math.max(0, InitialRawDueCount - RESTRICTED_MAX)) {
@@ -99,23 +111,20 @@ if (DEBUG) {console.log('[DEBUG - Ease Review Load] Body hidden') }
                         displayedDueCount = RESTRICTED_MAX; // Maybe set to 0 for user satisfaction until they click to continue reviewing?
                         debugLog('Due reviews still over threshold; reactivated reduction');
                     } else { // Deactivate
-                        /////////////////////////////////////////////////////////////////////////////////////////
-                        // I think this got triggered by the main function running again after doing the updates,
-                        // potentially by an observer that should have been disconnected?
-                        /////////////////////////////////////////////////////////////////////////////////////////
                         setItem('Activated', false);
                         Activated = false;
                         displayedDueCount = rawDueCount;
                         debugLog('Due count brought below threshold; deactivated reduction');
                     }
-                    if (rawDueCount > 0) showFinishedReducedPilePage(displayedDueCount);
+
+                    if (rawDueCount > 0) showFinishedSessionPage(displayedDueCount);
                     // otherwise the native reviews finished page will come up anyway
 
                 } else if (rawDueCount > InitialRawDueCount) {
                     // Artificially increase initial due count to avoid displayed count going above RESTRICTED_MAX
-                    debugLog('Due count exceeded that at time of activation; recalibrating reduction');
                     setItem('InitialRawDueCount', rawDueCount); // Don't need to update script copy of the variable as we won't use it again
                     displayedDueCount = RESTRICTED_MAX;
+                    debugLog('Due count exceeded that at time of activation; recalibrated reduction');
                 } else { // 0 < reduced review pile due count <= RESTRICTED_MAX
                     // Displayed due count changes will mirror raw due count changes relative to the initial raw due count
                     debugLog('Due count in expected range');
@@ -142,6 +151,7 @@ if (DEBUG) {console.log('[DEBUG - Ease Review Load] Body hidden') }
             // Remove the style element to restore visibility
             style.remove();
             debugLog('Body reinstated');
+
         }
     }
 
@@ -156,7 +166,7 @@ if (DEBUG) {console.log('[DEBUG - Ease Review Load] Body hidden') }
     main();
 
     function setUpFinishedPageTemplate() {
-        const bodyClone = document.querySelector('body').cloneNode(true);
+        const bodyClone = document.body.cloneNode(true);
         bodyClone.querySelector('div.nav a.nav-item > span').id = 'nav-dc-span';
         bodyClone.querySelector('div.container > h5').id = 'h5-well-done';
         for (const p of bodyClone.querySelectorAll('div.container > p:not(:last-of-type)')) { p.remove() }
@@ -164,15 +174,15 @@ if (DEBUG) {console.log('[DEBUG - Ease Review Load] Body hidden') }
         debugLog(`Finished page body innerHTML set: check localStorage.getItem('${localStoragePrefix}FinishedBlockHTML') to see it`);
     }
 
-    function showFinishedReducedPilePage(count) {
+    function showFinishedSessionPage(count) {
         const bodyHTML = getItem('FinishedBlockHTML');
-        document.querySelector('body').innerHTML = bodyHTML;
+        document.body.innerHTML = bodyHTML;
         document.getElementById('h5-well-done').innerText = `You've finished your ${RESTRICTED_MAX} card review block!`;
         document.getElementById('nav-dc-span').innerText = count;
         debugLog('Finished page updated');
     }
 
     // Fallback cleanup: Stop observing after a reasonable time
-    setTimeout(() => { if (docObserver) { docObserver.disconnect() } }, 5000);
+    setTimeout(() => docObserver.disconnect(), 5000);
 
 })();
