52f39be368a9cb1c848d2d059e1eba951425d3a1
Jai <108700712+JaiWWW@users.noreply.github.com>
2024-12-24 02:29:09 +0000

    Update script.user.js to use GM_[gs]etValue
    Decided it was better to use GM functions instead where available

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JPDB Ease Review Load
 // @namespace    http://tampermonkey.net/
-// @version      1.2.1
+// @version      1.3
 // @description  If you have lots of reviews due, make it look like you have less so it's easier to manage
 // @author       JaiWWW
 // @match        https://jpdb.io/*
@@ -11,6 +11,8 @@
 // @supportURL   https://github.com/JaiWWW/jpdb-ease-review-load/issues/new
 // @downloadURL  https://github.com/JaiWWW/jpdb-ease-review-load/raw/main/script.user.js
 // @updateURL    https://github.com/JaiWWW/jpdb-ease-review-load/raw/main/script.user.js
+// @grant        GM_getValue
+// @grant        GM_setValue
 // ==/UserScript==
 /* eslint-disable no-fallthrough -- fallthrough is love, fallthrough is life */
 
@@ -35,13 +37,14 @@ if (DEBUG) {console.log('[DEBUG - Ease Review Load] Body hidden') }
 
     function debugLog(msg) { if (DEBUG) console.log(`[DEBUG - Ease Review Load] ${msg}`) }
 
-    // Put my localStorage variables into a namespace by using these functions instead
-    const localStoragePrefix = 'EaseReviewLoad-';
-    const getItem = (name) => localStorage.getItem(`${localStoragePrefix}${name}`);
-    const setItem = (name, value) => localStorage.setItem(`${localStoragePrefix}${name}`, value);
+    try {GM_getValue; GM_setValue} catch {
+        // If GM functions are unavailable, map to script-identified localStorage variables instead
+        var GM_getValue = (name) => localStorage.getItem(`EaseReviewLoad-${name}`);
+        var GM_setValue = (name, value) => localStorage.setItem(`EaseReviewLoad-${name}`, value);
+    }
 
-    let Activated = getItem('Activated') === 'true';
-    let InitialRawDueCount = Number(getItem('InitialRawDueCount'));
+    let Activated = GM_getValue('Activated') === 'true';
+    let InitialRawDueCount = Number(GM_getValue('InitialRawDueCount'));
     let docObserver; // Observes initial document loading to update due counts as quickly as possible
     let navObserver; // Observes the nav element on review pages so the due counter on the back can be updated
 
@@ -60,15 +63,15 @@ if (DEBUG) {console.log('[DEBUG - Ease Review Load] Body hidden') }
             if (navDCSpan) {
                 docObserver.disconnect(); // To avoid double triggering while the function is running
                 debugLog('Nav due count element found, docObserver disconnected');
-    
+
                 const rawDueCount = navDCSpan.style.color === 'red' ? Number(navDCSpan.textContent) : 0;
                 let elementsToUpdate = [navDCSpan];
-    
+
                 // I thought I wouldn't get to use fallthrough in this script :D
                 const containerDCSpan = document.querySelector('div.container > p > span:not(:only-of-type)');
                 switch (url.slice(-7)) { // last 7 characters
                     case '/review':
-                        if (!getItem('FinishedBlockHTML')) setUpFinishedPageTemplate();
+                        if (!GM_getValue('FinishedBlockHTML')) setUpFinishedPageTemplate();
                     case 'jpdb.io':
                     case 'o/learn':
                         if (containerDCSpan) { // Doesn't exist except on the "You've finished your review session" page
@@ -76,7 +79,7 @@ if (DEBUG) {console.log('[DEBUG - Ease Review Load] Body hidden') }
                             elementsToUpdate.push(containerDCSpan);
                         }
                 }
-    
+
                 /* Old bad code which I replaced with the beautiful fallthrough above:
                 if (url.endsWith('.io') || url.endsWith('/learn')) {
                     // Span element in a p in the container holding the due count
@@ -89,15 +92,15 @@ if (DEBUG) {console.log('[DEBUG - Ease Review Load] Body hidden') }
                     if (containerDCSpan) { // Doesn't exist except on the "You've finished your review session" page
                         debugLog('Container due count element found (finished review session page)');
                         elementsToUpdate.push(containerDCSpan);
-                        if (!getItem('FinishedBlockHTML')) setUpFinishedPageTemplate();
+                        if (!GM_getValue('FinishedBlockHTML')) setUpFinishedPageTemplate();
                     }
                 }
                 */
-    
+
                 if (!Activated && rawDueCount > ACTIVATION_THRESHOLD) {
                     // ^ requirements for activating the due count reduction
-                    setItem('InitialRawDueCount', rawDueCount);
-                    setItem('Activated', true);
+                    GM_setValue('InitialRawDueCount', rawDueCount);
+                    GM_setValue('Activated', true);
                     InitialRawDueCount = rawDueCount;
                     Activated = true;
                     debugLog('Activated displayed due count reduction');
@@ -109,22 +112,22 @@ if (DEBUG) {console.log('[DEBUG - Ease Review Load] Body hidden') }
                         debugLog('Finished reduced review pile');
                         if (rawDueCount > ACTIVATION_THRESHOLD) {
                             // ^ requirement for re-initialising the due count reduction
-                            setItem('InitialRawDueCount', rawDueCount); // Don't need to update script copy of the variable as we won't use it again
+                            GM_setValue('InitialRawDueCount', rawDueCount); // Don't need to update script copy of the variable as we won't use it again
                             displayedDueCount = RESTRICTED_MAX; // Maybe set to 0 for user satisfaction until they click to continue reviewing?
                             debugLog('Due reviews still over threshold; reactivated reduction');
                         } else { // Deactivate
-                            setItem('Activated', false);
+                            GM_setValue('Activated', false);
                             Activated = false;
                             displayedDueCount = rawDueCount;
                             debugLog('Due count brought below threshold; deactivated reduction');
                         }
-    
+
                         if (rawDueCount > 0) showFinishedSessionPage(displayedDueCount);
                         // otherwise the native reviews finished page will come up anyway
-    
+
                     } else if (rawDueCount > InitialRawDueCount) {
                         // Artificially increase initial due count to avoid displayed count going above RESTRICTED_MAX
-                        setItem('InitialRawDueCount', rawDueCount); // Don't need to update script copy of the variable as we won't use it again
+                        GM_setValue('InitialRawDueCount', rawDueCount); // Don't need to update script copy of the variable as we won't use it again
                         displayedDueCount = RESTRICTED_MAX;
                         debugLog('Due count exceeded that at time of activation; recalibrated reduction');
                     } else { // 0 < reduced review pile due count <= RESTRICTED_MAX
@@ -132,7 +135,7 @@ if (DEBUG) {console.log('[DEBUG - Ease Review Load] Body hidden') }
                         debugLog('Due count in expected range');
                         displayedDueCount = RESTRICTED_MAX - (InitialRawDueCount - rawDueCount);
                     }
-    
+
                     // If user got to 0 displayed reviews and is now beneath ACTIVATION_THRESHOLD, we will have deactivated since
                     // the last time we checked Activated, so we need to check again to make sure we don't update when deactivated
                     if (Activated) {
@@ -142,18 +145,18 @@ if (DEBUG) {console.log('[DEBUG - Ease Review Load] Body hidden') }
                             element.textContent = displayedDueCount;
                         }
                         debugLog('Successfully overwrote counters');
-    
+
                         if (url.endsWith('/review')) {
                             navObserver.observe(navDCSpan, { characterData: true, subtree: true });
                             debugLog('navObserver set');
                         }
                     }
                 }
-    
+
                 // Remove the style element to restore visibility
                 style.remove();
                 debugLog('Body reinstated');
-    
+
             }
         } catch(e) {
             // Remove the style element to restore visibility
@@ -178,12 +181,12 @@ if (DEBUG) {console.log('[DEBUG - Ease Review Load] Body hidden') }
         bodyClone.querySelector('div.nav a.nav-item > span').id = 'nav-dc-span';
         bodyClone.querySelector('div.container > h5').id = 'h5-well-done';
         for (const p of bodyClone.querySelectorAll('div.container > p:not(:last-of-type)')) { p.remove() }
-        setItem('FinishedBlockHTML', bodyClone.innerHTML);
-        debugLog(`Finished page body innerHTML set: check localStorage.getItem('${localStoragePrefix}FinishedBlockHTML') to see it`);
+        GM_setValue('FinishedBlockHTML', bodyClone.innerHTML);
+        debugLog(`Finished setting up page body innerHTML`);
     }
 
     function showFinishedSessionPage(count) {
-        const bodyHTML = getItem('FinishedBlockHTML');
+        const bodyHTML = GM_getValue('FinishedBlockHTML');
         document.body.innerHTML = bodyHTML;
         document.getElementById('h5-well-done').innerText = `You've finished your ${RESTRICTED_MAX} card review block!`;
         document.getElementById('nav-dc-span').innerText = count;
